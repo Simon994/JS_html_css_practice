47cc479b5376aa2ed53ddbea3efbb3cce316b19b
Simon994 <65023744+Simon994@users.noreply.github.com>
2020-07-14 11:20:08 +0100

    Works with strings and returns single array

--- Heaps_Algo/Heaps_Algo.js
+++ Heaps_Algo/Heaps_Algo.js
@@ -33,65 +33,18 @@ function permutations(str) {
   }
 
   const result = function (input){
-    console.log(input)
+    permutedArr.push(input.join(''));
   }
 
   heapPermutations(arr, result);
- 
+  permutedArr = [...new Set(permutedArr)];
+  return(permutedArr);
 
 }
 
 
-permutations('abcd');
+permutations('aac');
 
 
 
 
-
-
-
-// function permutations(str) {
-  
-//   const arr = str.split('');
-//   const permutationsArr = []; 
-//    if (arr.length === 1) return arr;
-
-//   // for (let j=0; j<arr.length; j++){
-//     for(let i=0; i<arr.length-1; i++){
-//       swap(i, i+1);
-//       console.log(arr);
-//        // permutationsArr.push(arr.join(''));
-//     }
-//     for (let k=(arr.length-1); k>0; k--){
-//        swap(k, k-1);
-//         console.log(arr);
-//        //permutationsArr.push(arr.join(''));
-//     }
-
-//   // }
-
-//   function swap(a, b){
-//     if(arr[a] === arr[b]) return;
-//     let temp = arr[a];
-//     arr[a] = arr[b];
-//     arr[b] = temp;
-//     return arr
-
-//   }
- 
-//   const removedDuplicates = [...new Set (permutationsArr)];
-//   return (removedDuplicates);
-// }
-
-
-
-
-
-
-
-
-
-
-
-// '[\'abcd\', \'abdc\', \'acbd\', \'acdb\', \'adbc\', \'adcb\', \'bacd\', \'badc\', \'bcad\', \'bcda\', \'bdac\', \'bdca\', \'cabd\', \'cadb\', \'cbad\', \'cbda\', \'cdab\', \'cdba\', \'dabc\', \'dacb\', \'dbac\', \'dbca\', \'dcab\', \'dcba\']', 
-// instead got: '[\'abcd\', \'abdc\', \'adbc\', \'bacd\', \'bcad\', \'bcda\', \'cbda\', \'cdab\', \'cdba\', \'dabc\', \'dacb\', \'dcab\']'
