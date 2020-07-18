f5dbdfa8d6696b3066f4d874929249ddac2b0142
Simon994 <65023744+Simon994@users.noreply.github.com>
2020-07-18 08:32:23 +0100

    currently finds a larger number, but not necessarily the next largest

--- Next_biggest_num_with_same_digits/next_biggest_same_digits.js
+++ Next_biggest_num_with_same_digits/next_biggest_same_digits.js
@@ -3,13 +3,14 @@
 
 function nextBigger(num){
   const arr = num.toString().split('');
-
+  let result;
 
   function compare(arr, p = arr.length-1){
     let l = p-1;
+    console.log(p);
     if(p === 0) {
-      console.log(-1);
-      return -1;
+      //console.log(-1);
+      return result = -1;
     }
 
 
@@ -19,103 +20,40 @@ function nextBigger(num){
       let moving = tempArr[p];
       tempArr[p] = tempArr[i];
       tempArr[i] = moving;
-      //console.log(`tried swapping pointer ${p} with left-bound. temp array: ${tempArr}`);
+      console.log(`tried swapping pointer ${p} with left-bound: ${i}. temp array: ${tempArr}`);
       if (tempArr.join('') > arr.join('')){
-        console.log(`found greater number, with p at ${p}. Number: ${tempArr.join('')}`);
-        return parseInt(tempArr.join(''),10)
-      } 
+        console.log(`found greater number, with p at ${p}.left-bound: ${i} Number: ${tempArr.join('')}`);
+        return result = parseInt(tempArr.join(''),10);
+      } else if (tempArr.join('') === arr.join('')) {
+        console.log('tempArr and original arr are the same, breaking out of loop and moving to next p.');
+        break;
+      }
 
     }
 
     //console.log(`swaps fully tried at p: ${p}. Moving pointer to: ${p-1}`);
 
     compare(arr, p-1)
-
-    //base case
+    
    
 
   }
 
 
   compare(arr);
-
+  return result;
 
 
 }
 
-nextBigger(144); // should be 414
-//nextBigger(414); // should be 441
+//nextBigger(6633750298); //Expected: 6633750829
 
-// function nextBigger(num){
-
-//   const arr = num.toString().split('');
-//   const arrOriginal = num.toString();
-//   let length = arr.length-1;
-
-//   for (i=1; i<arr.length; i++){
-//     console.log(`number: ${arr.join('')}. swapping index ${length} (number: ${arr[length]}) and index ${length-i} (number: ${arr[length-i]})`)
-//     let moving = arr[length];
-//     arr[length] = arr[length-i];
-//     arr[length-i] = moving;
-    
-//     if (arr.join('') > arrOriginal){
-//       console.log(arr.join(''));
-//     } else {
-//       console.log(`new number (${arr.join('')}) is NOT greater than original`)
-
-//     }
+nextBigger(626741); // Expected: 627146
+//console.log(nextBigger(10));
+//nextBigger(350732); //Expected: 352037
+//console.log(nextBigger(144)); // should be 414
+//console.log(nextBigger(4144)); // should be 4414
+//console.log(nextBigger(414)); // should be 441
+//console.log(nextBigger(2017));
 
 
-//   }
-
-
-// }
-
-// //extBigger(531);
-// nextBigger(1431);
-
-
-
-
-
-
-
-
-
-// function nextBigger(num) {
-
-//   const arr = num.toString().split('');
-//   let permutedArr = [];
-//   function swap(arr, a, b){
-//     let temp = arr[a];
-//     arr[a] = arr[b];
-//     arr[b] = temp;
-//     return arr;
-//   }
-
-//   function heapPermutations(arr, n = arr.length){
-//     if (n===1){
-//       permutedArr.push(arr.join(''));;
-//     } else {
-//       for (let i=1; i<=n; i++){
-        
-//         heapPermutations(arr, n-1);
-//         if(n%2){
-//           swap(arr, 0, n-1);
-//         } else {
-//           swap(arr, i-1, n-1);
-//         }
-//       }
-//     }
-
-//   }
-
-//   heapPermutations(arr);
-//   permutedArr = [...new Set(permutedArr)];
-//   let indexOfNum = permutedArr.sort().indexOf(num.toString());
-//   return parseInt(permutedArr[indexOfNum+1], 10);
-
-// }
-
-
-// console.log(nextBigger(2017));
